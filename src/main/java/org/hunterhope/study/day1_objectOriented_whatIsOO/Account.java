1c5b94a22ca40bfc8077fea313408366c77a73b6
Hank <hunterhope@outlook.com>
2022-03-16 00:45:03 +0800

    OCP_day1

---
+++ src/main/java/org/hunterhope/study/day1_objectOriented_whatIsOO/Account.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.hunterhope.study.day1_objectOriented_whatIsOO;
+
+/**
+ *
+ * @author user
+ */
+public class Account {
+
+    String name;
+    int balance;
+
+    /**
+     * 存款程序
+     *
+     * @param amount 存款金額
+     */
+    public void diposit(int amount) {
+        if (amount <= 0) {
+            System.out.println("金額不可以是負的或零");
+            return;
+        }
+        balance += amount;
+    }
+
+    /**
+     * 提款程序
+     *
+     * @param amount 提款金額
+     */
+    public void withdrawal(int amount) {
+        if (amount < 0) {
+            System.out.println("金額不可以是負的");
+            return;
+        }
+
+        if (balance >= amount) {
+            balance -= amount;
+        } else {
+            System.out.println("餘額不足");
+        }
+
+    }
+
+    /**
+     * 打印程序
+     */
+    public void print() {
+        System.out.printf("%s %d%n", name, balance);
+    }
+}
---
+++ src/main/java/org/hunterhope/study/day1_objectOriented_whatIsObject/Bmi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.hunterhope.study.day1_objectOriented_whatIsObject;
+
+/**
+ *
+ * @author user
+ */
+public class Bmi {
+    String name;
+    double height;
+    double weight;
+    double bmi;
+}
