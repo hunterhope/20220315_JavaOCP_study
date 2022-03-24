c0f891bcac7476d4fc05dd671296ffa0a9ab94c0
Hank <hunterhope@outlook.com>
2022-03-25 00:25:03 +0800

    OCP_day4

---
+++ src/main/java/org/hunterhope/study/day4_ArrayAndProgramming/test/TestTTTGame_userDoIt.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package org.hunterhope.study.day4_ArrayAndProgramming.test;
+
+import org.hunterhope.study.day4_ArrayAndProgramming.TTTGame;
+
+/**
+ *
+ * @author user
+ */
+public class TestTTTGame_userDoIt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TTTGame.clear();
+        TTTGame.print();
+        TTTGame.userDoIt();
+        TTTGame.userDoIt();
+    }
+    
+}
